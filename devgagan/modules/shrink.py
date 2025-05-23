1dc1b14f0fbf405cf38d9402531140dc150c748c
gthub666 <ankittkk25@gmail.com>
2025-05-23 14:09:03 +0530

    Update shrink.py

--- devgagan/modules/shrink.py
+++ devgagan/modules/shrink.py
@@ -77,9 +77,7 @@ async def start_handler(client, message):
         await message.reply_photo(
             msg.photo.file_id,
             caption=(
-                "Hi 👋 Welcome, Wanna intro...?\n\n"
-                "✳️ I can save posts from channels or groups where forwarding is off. I can download videos/audio from YT, INSTA, ... social platforms\n"
-                "✳️ Simply send the post link of a public channel. For private channels, do /login. Send /help to know more."
+                "Hi!"
             ),
             reply_markup=keyboard
         )
