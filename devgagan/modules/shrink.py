2e5aeb206c9c0dfd9925d96bbc29a2cdb4e6d05f
gthub666 <ankittkk25@gmail.com>
2025-05-23 14:01:26 +0530

    Update shrink.py

--- devgagan/modules/shrink.py
+++ devgagan/modules/shrink.py
@@ -26,23 +26,82 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from config import MONGO_DB, WEBSITE_URL, AD_API, LOG_GROUP  
  
  
+tclient = AsyncIOMotorClient(MONGO_DB)
+tdb = tclient["telegram_bot"]
+token = tdb["tokens"]
+ 
+ 
+async def create_ttl_index():
+    await token.create_index("expires_at", expireAfterSeconds=0)
+ 
+ 
+ 
+Param = {}
+ 
+ 
+async def generate_random_param(length=8):
+    """Generate a random parameter."""
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+ 
+ 
+async def get_shortened_url(deep_link):
+    api_url = f"https://{WEBSITE_URL}/api?api={AD_API}&url={deep_link}"
+ 
+     
+    async with aiohttp.ClientSession() as session:
+        async with session.get(api_url) as response:
+            if response.status == 200:
+                data = await response.json()   
+                if data.get("status") == "success":
+                    return data.get("shortenedUrl")
+    return None
+ 
+ 
+async def is_user_verified(user_id):
+    """Check if a user has an active session."""
+    session = await token.find_one({"user_id": user_id})
+    return session is not None
+ 
+ 
 @app.on_message(filters.command("start"))
 async def token_handler(client, message):
-    """Handle the /start command."""
+    """Handle the /token command."""
     join = await subscribe(client, message)
     if join == 1:
         return
-
+    chat_id = "save_restricted_content_bots"
+    msg = await app.get_messages(chat_id, 796)
     user_id = message.chat.id
+    if len(message.command) <= 1:
+        image_url = "https://i.postimg.cc/v8q8kGyz/startimg-1.jpg"
+        join_button = InlineKeyboardButton("Join Channel", url="https://t.me/team_spy_pro")
+        premium = InlineKeyboardButton("Get Premium", url="https://t.me/kingofpatal")   
+        keyboard = InlineKeyboardMarkup([
+            [join_button],   
+            [premium]    
+        ])
+         
+        await message.reply_photo(
+            msg.photo.file_id,
+            caption=(
+                "Hi 👋 Welcome, Wanna intro...?\n\n"
+                "✳️ I can save posts from channels or groups where forwarding is off. I can download videos/audio from YT, INSTA, ... social platforms\n"
+                "✳️ Simply send the post link of a public channel. For private channels, do /login. Send /help to know more."
+            ),
+            reply_markup=keyboard
+        )
+        return  
+ 
     param = message.command[1] if len(message.command) > 1 else None
-
     freecheck = await chk_user(message, user_id)
     if freecheck != 1:
         await message.reply("You are a premium user no need of token 😉")
         return
-
+ 
+     
     if param:
         if user_id in Param and Param[user_id] == param:
+             
             await token.insert_one({
                 "user_id": user_id,
                 "param": param,
@@ -55,7 +114,7 @@ async def token_handler(client, message):
         else:
             await message.reply("❌ Invalid or expired verification link. Please generate a new token.")
             return
-
+ 
 @app.on_message(filters.command("token"))
 async def smart_handler(client, message):
     user_id = message.chat.id
