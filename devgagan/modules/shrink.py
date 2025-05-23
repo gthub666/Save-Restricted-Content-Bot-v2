8f2ba52f3a697327ae9b41e8b6a3a8b6cb0c5ad8
gthub666 <ankittkk25@gmail.com>
2025-05-23 13:50:48 +0530

    Update shrink.py

--- devgagan/modules/shrink.py
+++ devgagan/modules/shrink.py
@@ -27,6 +27,61 @@ from config import MONGO_DB, WEBSITE_URL, AD_API, LOG_GROUP
  
  
 @app.on_message(filters.command("start"))
-async def start_handler(client, message):
-    if await subscribe(client, message) == 1:
+async def token_handler(client, message):
+    """Handle the /start command."""
+    join = await subscribe(client, message)
+    if join == 1:
         return
+
+    user_id = message.chat.id
+    param = message.command[1] if len(message.command) > 1 else None
+
+    freecheck = await chk_user(message, user_id)
+    if freecheck != 1:
+        await message.reply("You are a premium user no need of token 😉")
+        return
+
+    if param:
+        if user_id in Param and Param[user_id] == param:
+            await token.insert_one({
+                "user_id": user_id,
+                "param": param,
+                "created_at": datetime.utcnow(),
+                "expires_at": datetime.utcnow() + timedelta(hours=3),
+            })
+            del Param[user_id]   
+            await message.reply("✅ You have been verified successfully! Enjoy your session for next 3 hours.")
+            return
+        else:
+            await message.reply("❌ Invalid or expired verification link. Please generate a new token.")
+            return
+
+@app.on_message(filters.command("token"))
+async def smart_handler(client, message):
+    user_id = message.chat.id
+     
+    freecheck = await chk_user(message, user_id)
+    if freecheck != 1:
+        await message.reply("You are a premium user no need of token 😉")
+        return
+    if await is_user_verified(user_id):
+        await message.reply("✅ Your free session is already active enjoy!")
+    else:
+         
+        param = await generate_random_param()
+        Param[user_id] = param   
+ 
+         
+        deep_link = f"https://t.me/{client.me.username}?start={param}"
+ 
+         
+        shortened_url = await get_shortened_url(deep_link)
+        if not shortened_url:
+            await message.reply("❌ Failed to generate the token link. Please try again.")
+            return
+ 
+         
+        button = InlineKeyboardMarkup(
+            [[InlineKeyboardButton("Verify the token now...", url=shortened_url)]]
+        )
+        await message.reply("Click the button below to verify your free access token: \n\n> What will you get ? \n1. No time bound upto 3 hours \n2. Batch command limit will be FreeLimit + 20 \n3. All functions unlocked", reply_markup=button)
